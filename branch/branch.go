b81908419bcbc932e4c748b8f95dd6824e5aab65
Jerry Zhu <zhurq0321@gmail.com>
2021-01-26 13:05:24 +0800

    finished ver

--- branch/branch.go
+++ branch/branch.go
@@ -5,11 +5,43 @@ import (
 	"go/parser"
 	"go/token"
 )
-
+//func Inspect(node Node, f func(Node) bool)
+//Inspect traverses an AST in depth-first order: It starts by calling f(node); node must not be nil. 
+//If f returns true, 
+//Inspect invokes f recursively for each of the non-nil children of node, followed by a call of f(nil).
+//https://golang.org/pkg/go/ast/#Inspect
 func branchCount(fn *ast.FuncDecl) uint {
 	// TODO: Write the branchCount function,
 	// count the number of branching statements in function fn
-	return 12345
+	//https://play.golang.org/p/cq2OI6CA6v_n
+	var Count uint = 0
+	
+	ast.Inspect(fn, func (node ast.Node) bool{
+		//10-15lines
+		//detect if for switch range, type switch, goto, continue, break, fallthrough
+		// If we return true, we keep recursing under this AST node.
+        // If we return false, we won't visit anything under this AST node.
+		switch node.(type) {
+		case *ast.IfStmt: //find if
+			Count++
+		case *ast.ForStmt: //find for
+			Count++
+		case *ast.SwitchStmt: //find switch
+			Count++
+		case *ast.RangeStmt: //find Range
+			Count++
+		case *ast.TypeSwitchStmt: //find type
+			Count++
+		case *ast.BranchStmt: //include goto, continue, break, fallthrough
+			Count++
+		// default:
+		// 	return false
+		}
+	
+	return true
+})
+
+	return Count
 }
 
 // ComputeBranchFactors returns a map from the name of the function in the given
