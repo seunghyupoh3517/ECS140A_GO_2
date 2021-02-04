160802ab934a8d51c7179832389f0ed431e9a30e
Draco Chen <51469668+draco168@users.noreply.github.com>
2021-02-04 15:51:29 -0800

    Update parser.go
    Co-Authored-By: WeiyiZ-008 <59705192+WeiyiZ-008@users.noreply.github.com>

--- term/parser.go
+++ term/parser.go
@@ -30,82 +30,104 @@ type Grammar struct {
 
 // NewParser creates a struct of a type that satisfies the Parser interface.
 func NewParser() Parser {
-	var parseGrammar Parser := Grammar{make(map[*Token]*Term)}
+	var parseGrammar Parser = Grammar{make(map[*Term][]*Term)}
 	return parseGrammar
 }
 
+// equivalent grammar
+// <term> ::= ATOM <new> | NUM | VAR
+// <new> ::= nil | ( <args> )
+// <args> ::= <term> <new2>
+// <new2> ::= nil | , <args>
 
-// what dose Parse need to do?
-// it receives a string, we need to convert it to the term type, Otherwise, return a error for invalid string given to us
-// we can apply the lexer to separate the string to tokens
-// For converting to the term, we need to figure out several attributes and assign to the Term struct
-	// term type
-	// content
-	// functor, the name of the function
-	// args
-	// use term_test as reference, it shows how to initialize a Term object
-
+// nonTerminal enumerates all types to non terminal
 type nonTermial int
 
 const (
-	Start nonTermial = iota
-	Term 
-	New 
-	Compound 
-	Funct 
-	Argus 
-	New'
-	// arr[S][atom]
+	Start_NT nonTermial = iota
+	Term_NT
+	NT1
+	Args_NT
+	NT2
 )
 
+// implements the Parse method with Grammar struct
 func (g Grammar) Parse(str string) (*Term, error) {
 	// TODO: matrix in the global
-	var parseTable [][]int
+	// The parseTable can be the type of
+	// []interface{} which is the list in a single cell
+	var parseTable [][][]interface{}
 
 	lex := newLexer(str)
 
 	// convert the input string to tokens
 	var	tokenList []*Token
-	for token, err := lex.next(); err != ErrLexer {
-		tokenList.append(token)
-	} else {
-		// validating the given string, return error if can't parse to token
-		// TODO: double check with error object return type
-		return nil, fmt.Errorf(lex.ErrLexer)
+	for {
+		token, err := lex.next()
+		if err == ErrLexer {
+			// validating the given string, return error if can't parse to token
+			// TODO: double check with error object return type
+			return nil, err
+			} else {
+				if token.typ == tokenEOF {
+					break
+				}
+				tokenList = append(tokenList, token)
+			}
 	}
 
+
 	// [ "bar", "(", "x", ")" ]
-	// pointer point to the current token
+	// pointer point to the current token in the list
 	var tokenInd = 0
+
 	// initialize the stack
-	var stack []nonTermial
-	stack.append(Start)
- 	while (len(stack) != 0 ) {
- 		ind := len(stack) - 1		// ind 
- 		topOfStack := stack[ind]
+	// stack needs to accept two data types, nonTerminal & tokenType
+	var stack []interface{}
+	stack = append(stack, Start_NT)
+
+ 	for len(stack) != 0 {
+ 		ind := len(stack) - 1		// index of top element in the stack
+ 		topOfStack := stack[ind]	// topOfStack is the tokenType or nonTerminal
 
  		switch typ := topOfStack.(type) {
  		case tokenType:
  			// when the top is terminal
- 			// pop out the terminal and advance the tokenList index 
+ 			// pop out the terminal and advance the tokenList index
  			// when the tokenType(terminal) are the same
- 			if tokenList[tokenInd].typ == topOfStack.typ {
+ 			if tokenList[tokenInd].typ == topOfStack {
  				stack = stack[:ind]		// pop out the top element
- 				tokenInd++;	
+ 				tokenInd += 1;
  			} else {
  				// terminal is not match
- 				return nil, fmt.Errorf("Invalid input string")
+ 				// TODO: double check here
+ 				return nil, ErrParser
  			}
 
- 		case nonTermial: 
+ 		case nonTermial:
  			// when the top is non terminal
- 			if parseTable[topOfStack][]
+ 			// check the value in the parsing table with given token
+ 			if parseTable[topOfStack][tokenList[tokenInd]] != nil {
+ 				// value inside the cell, find the transition to other state
+ 				var transList = parseTable[topOfStack][tokenList[tokenInd]]
+ 				var listIndex = len(transList) -1
+
+ 				stack = stack[:ind]		// pop out the top non terminal before push
+ 				// push T -> X1 X2 X3 to the stack in reverse order
+ 				for listIndex >= 0 {
+	 				stack = append(stack, transList[listIndex])
+					listIndex -= 1
+	 			}
+ 			} else {
+ 				// no value at given cell, invalid input string
+ 				return nil, ErrParser
+ 			}
 
  		default:
  			// invalid type in the stack
- 			return nil, fmt.Errorf("Invalid type in the stack")
+ 			return nil, ErrParser
  		}
  	}
 
-
+	return nil, nil
 }
